37898a7a092c4d1eec38a8efbef4a7d3cbe1a8e3
noptiker <StepanKud513@yandex.ru>
2026-04-26 23:23:10 +0400

    Update dfs.cpp

--- dfs/dfs.cpp
+++ dfs/dfs.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include<queue>
+#include<stack>
 
+using namespace std;
+
+void dfs(const vector<vector<int>>& grsm, int start) {//в глубину через стек
+	int si = grsm.size();
+	vector<bool> v(si, false);
+	stack<int> st;
+	st.push(start);
+	v[start] = true;
+	while (!st.empty()) {
+		int tek = st.top();
+		st.pop();
+		cout << tek << " ";
+		for (int sos : grsm[tek]) {
+			if (!v[sos]) {
+				st.push(sos);
+				v[sos] = true;
+			}
+		}
+	}
+}
